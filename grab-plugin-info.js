52952ead3eacaf4836cad7aae4f36ff566bbd6db
Viridiris <josephbetbeze@gmail.com>
2021-09-27 18:07:21 -0400

    Setup first js file

---
+++ grab-plugin-info.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Grab WP Plugin info
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        */wp-admin/plugins.php
+// @require http://code.jquery.com/jquery-3.6.0.min.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    $( document ).ready(function() {
+
+    });
+})();
